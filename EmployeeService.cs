add52723931c31ec4bf781fc827b81ae87b1ac47
thomasfregene <fregzythomas@gmail.com>
2020-11-01 15:38:51 +0100

    Implementing the getEmployee service

---
+++ EmployeeService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class EmployeeService : IEmployeeService
+    {
+        public Employee GetEmployee(int id)
+        {
+            Employee employee = new Employee();
+
+            string cs = ConfigurationManager
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
